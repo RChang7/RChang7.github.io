971f233e29a1b09d4fdb7fb1918f9de91f65ff1d
cRanial-dySfunCtion <146732694+cRanial-dySfunCtion@users.noreply.github.com>
2025-12-23 01:25:42 +1000

    added dark mode

--- src/About.js
+++ src/About.js
@@ -2,12 +2,16 @@ const About = () => {
     return ( 
         <div className="about-page">
             <h2>About Me</h2>
-            <p>Placeholder Text</p>
-            
+            <p>Placeholder Text Placeholder Text Placeholder Text Placeholder Text 
+                Placeholder Text Placeholder Text Placeholder Text Placeholder Text 
+                Placeholder Text Placeholder Text Placeholder Text Placeholder Text 
+                Placeholder Text</p>
+
+
+            <br />
             <a href={"https://github.com/RChang7"}>Github</a>
             <br />
             <a href={"https://www.linkedin.com/in/ryanchang-it/"}>LinkedIn</a>
-            
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,28 @@
-import {Link} from 'react-router-dom';
-
+import { Link} from 'react-router-dom';
+import { useState } from 'react';
 
 
 
 
 const Navbar = () => {
-    return (  
+
+    // false is light mode, true is dark mode
+    const [mode, setMode] = useState(false);
+
+    const switchMode = () => {
+        setMode(!mode);
+    };
+
+    return (
         <nav className="navbar">
             <h1>Ryan Chang</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/allrepos">All Repos</Link>
                 <Link to="/about">About Me</Link>
+                {/* <button onClick = {switchMode} className="mode-switcher">
+                    {mode ? "Light Mode" : "Dark Mode"}
+                </button> */}
             </div>
         </nav>
     );
