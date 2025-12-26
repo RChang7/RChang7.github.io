cf39cbc76e0e6829c30aec8d1949051a535c2fd7
cRanial-dySfunCtion <146732694+cRanial-dySfunCtion@users.noreply.github.com>
2025-12-26 21:38:50 +1000

    fixed footer not sticking to bottom on short pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,4 @@
 import { Link} from 'react-router-dom';
-import { useState } from 'react';
 
 
 
@@ -14,7 +13,7 @@ const Navbar = ({isDarkMode, toggleThemeSwitch}) => {
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/allrepos">Projects</Link>
-                <Link to="/about">About Me</Link>
+                <Link to="/about">About</Link>
                 <button onClick = {toggleThemeSwitch} className="mode-switcher">
                     {isDarkMode ? "Dark Mode" : "Light Mode"}
                 </button>
