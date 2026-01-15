03de1543e2726c7917c5a7490180fabfb6b89780
cRanial-dySfunCtion <146732694+RChang7@users.noreply.github.com>
2026-01-15 22:56:09 +1000

    updated backend

--- backend/backend.js
+++ backend/backend.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from "cors";
+
 const app = express();
 const port = 5000;
+const cors_option = {
+    origin: 'http://ryanstchang.dev', 
+    optionsSuccessStatus: 200
+}
 
 
+app.use(cors(cors_option));
 
 app.get('/repos', (req, res) => {
     fetch("https://api.github.com/users/RChang7/repos")
