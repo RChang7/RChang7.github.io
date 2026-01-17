324cd65676485a9ef07885be83b0f5a8f2996b27
cRanial-dySfunCtion <146732694+RChang7@users.noreply.github.com>
2026-01-17 18:14:13 +1000

    backend changes

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 
 const app = express();
-const port = 80;
+const port = 3000;
 const cors_option = {
     origin: ['http://ryanstchang.dev', 'http://localhost:3000'], 
     optionsSuccessStatus: 200
